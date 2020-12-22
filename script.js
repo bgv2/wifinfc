b1252c049926eeae7064e6712d019bc3c3eba702
ed789d0 <26331505+ed789d0@users.noreply.github.com>
2020-12-22 18:34:50 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+new ClipboardJS(".btn");
+let url = new URL(window.location.href);
+
+function blankpage() {
+  window.location.replace("about:blank");
+}
+
+let ssid = url.searchParams.get("ssid") || "";
+if (ssid) ssid = decode(ssid);
+document.querySelector(".ssid").innerHTML = ssid;
+
+function al() {
+  let c = url.searchParams.get("pw");
+  if (c) {
+    let response = c;
+    response = decode(response);
+    // fill in input field with decoded password
+    document.getElementById("bar").value = response;
+  } else {
+    document.getElementById("bar").value =
+    "error: password not copied properly";
+  }
+}
+
+function encode(str) {
+  str.replaceAll("+", "~");
+  str.replaceAll("/", "_");
+  return btoa(encodeURIComponent(str));
+}
+
+function decode(str) {
+  str.replaceAll("~", "+");
+  str.replaceAll("_", "/");
+  return decodeURIComponent(atob(str));
+}
